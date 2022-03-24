d151ad7516e678316798342092d9d72ff8c96b6c
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2022-03-24 20:46:53 +0000

    Personalized the chat bot with Name entry, now trying to make a 9Gag bomb but chrome keeps blocking the pop ups hehe

--- js/scripts.js
+++ js/scripts.js
@@ -58,16 +58,36 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+// Tiny lil meme - 
 function callAgainAndAgain() {
     let name = prompt("What's your name?");
-    alert(name + " is a stupid name, lol. Hector Jones is way cooler!");
+    alert(name + " is a dumbass name, ¯\\_(ツ)_/¯ Hector Jones is way cooler!");
+    for(var i = 0; i < 10; i++)
+    {
+        window.open('https://9gag.com', '_blank');
+        setTimeout(function(){
+            window.open('https://9gag.com', '_blank');
+          }, 1000);
+    }
 }
 
+// Big meme - chat bot
 function initChat(){
     var messages = document.getElementById('messages');
+    let child_nodes = messages.childNodes;
+    //console.log(child_nodes.length);
     var form = document.getElementById('form');
     var input = document.getElementById('input');
     var chat_received = false;
+
+    // Get the name for personalizing the chat bot
+    var item = document.createElement('li');
+    item.textContent = "Hi there, what's your name partner?";
+    messages.appendChild(item);
+
+    // TO DO: Get age, or other personalizing characteristics?
+
+    // Bot responses
     const responses = [ "Every minute, 60 seconds pass in Africa!", 
                         "What a stupid thing to say. You is dumb!",
                         "I've never even shot a chicken in the foot before",
@@ -92,30 +112,110 @@ function initChat(){
                         "Well, just grab my ears while you fuck my butt!",
                     ]
 
+    // Our personalizing additives
+    var response_additives;
+    var endings;
+    var counter = 0;
+
     form.addEventListener('submit', function(e) {
         e.preventDefault();
+        // Check if input there to send as chat
         if (input.value) {
             var item = document.createElement('li');
             item.textContent = input.value;
+
+            // Get their name and save it in the var
+            if(child_nodes.length == 1){
+                const name = input.value;
+                response_additives = [  "Well " + name + ", ", 
+                                        "",
+                                        "Did I ask you, " + name + "? ",
+                                        "",
+                                        "",
+                                    ];
+                endings = [ "Dude, " + name + ", you need to chill the fuck out.",
+                            name + " , you know, you're not getting any younger.",
+                            "Well wouldn't you like to play with my nipples huh?",
+                            "I like to eat garbage honestly. I am garbage honestly.",
+                            "Yeah alright, fine you got me, " + name + ". There is no BOT. Just this fine ass BOOTY!"];
+            }
+            console.log(child_nodes.length);
+            // Post all normal chats to the wall and reset input
             messages.appendChild(item);
             window.scrollTo(0, document.body.scrollHeight);
+
+            // Set flag
             chat_received = true;
             input.value = '';
         }
+
+        // If we have something from the user
         if(chat_received){
-            var item = document.createElement('li');
-            console.log(responses.length);          
+
+            // Check first if we still have responses to process
             if(responses.length > 0){
+                var item = document.createElement('li');
                 var index = Math.floor(Math.random() * responses.length);
-                item.textContent = responses[index];
+
+                if(typeof response_additives != "undefined"){
+                    var index2 = Math.floor(Math.random() * response_additives.length);
+                }
+
+                console.log(response_additives[index2]);
+                item.textContent = response_additives[index2] + responses[index];
                 responses.splice(index, 1);
                 messages.appendChild(item);
                 window.scrollTo(0, document.body.scrollHeight);
             }
+
+            // If not, tell Hector to fuck off <3
+            // Also jokes aside, could have used a switch here awkward Marc
             else if(responses.length <= 0){
-                item.textContent = "Yeah alright, fine you got me. There is no BOT. Just this fine ass BOOTY!";
-                messages.appendChild(item)
+                counter = counter - 1;
+                console.log(counter);
+                if(counter >=-1){
+                    var item = document.createElement('li');
+                    item.textContent = "WHAT?";
+                    messages.appendChild(item);
+                }
+                else if(counter >= -2){
+                    var item = document.createElement('li');
+                    item.textContent = "WHAT? DID YOU JUST FUCKING SAY TO ME?";
+                    messages.appendChild(item);
+                }
+                else if(counter >= -3){
+                    var item = document.createElement('li');
+                    item.textContent = endings[0];
+                    messages.appendChild(item);
+                }
+                else if(counter >= -4){
+                    var item = document.createElement('li');
+                    item.textContent = endings[1];
+                    messages.appendChild(item);
+                }
+                else if(counter >= -6){
+                    var item = document.createElement('li');
+                    item.textContent = endings[2];
+                    messages.appendChild(item);
+                }
+                else if(counter >= -8){
+                    var item = document.createElement('li');
+                    item.textContent = endings[3];
+                    messages.appendChild(item);
+                }
+                else if(counter >= -100){
+                    var item = document.createElement('li');
+                    item.textContent = endings[4];
+                    messages.appendChild(item);
+                }
+                else if(counter >= -1000){
+                    var item = document.createElement('li');
+                    item.textContent = "Bro if you got here, what the fuck?";
+                    messages.appendChild(item);
+                }
             }
+
+            // Reset the flag
             chat_received = false;
         }
     });
